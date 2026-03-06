07fe00bc2707413f26b8466bc81fdc32b397ac57
GustasNa <gnavierauskas@gmail.com>
2026-03-06 22:30:09 +0200

    Update v.pradinis.cpp

--- v.pradinis.cpp
+++ v.pradinis.cpp
@@ -1,76 +1,5 @@
 #include <bits/stdc++.h>
-using namespace std;
-
-
-int s_int(){
-    while(true){
-        try{
-            int x;
-            cin>>x;
-            return x;
-        }
-        catch(const ios::failure&){
-        cin.clear();
-        string kld;
-        getline(cin, kld);
-        cout<<"Klaida: iveskite skaiciu"<<endl;
-        }
-    }
-}
-
-double s_double(){
-    while(true){
-        try{
-            double x;
-            cin>>x;
-            return x;
-        }
-        catch (const ios::failure&){
-            cin.clear();
-            string kld;
-            getline(cin, kld);
-            cout<<"Klaida: iveskite skaiciu"<<endl;
-        }
-    }
-
-}
-
-struct stud {
-    string vardas;
-    string pavarde;
-    vector<double> tarp;
-    double egz;
-    double vid;
-    double med;
-};
-
-double atsitiktinis() {
-    return rand()%11;
-}
-
-void skaiciuoti(stud &s) {
-    double sum=s.egz;
-    for (double x:s.tarp) sum += x;
-    s.vid=sum/(s.tarp.size()+1);
-
-    sort(s.tarp.begin(), s.tarp.end());
-    int d=s.tarp.size();
-    if (d%2==0)
-        s.med=(s.tarp[d/2]+s.tarp[d/2-1])/2.0;
-    else
-        s.med=s.tarp[d/2];
-}
-
-void spausdinti_lentele(const vector<stud>& A){
-    cout<<endl<<left<<setw(15)<<"Pavarde"<<left<<setw(15)<<"Vardas"<<right<<setw(20)<<"Galutinis (Vid.)"<<right<<setw(20)<<"Galutinis (Med.)"<<endl;
-
-    cout<<string(70, '-')<<endl;
-
-    for(const auto &s : A){
-        cout<<left<<setw(15)<<s.pavarde<<left<<setw(15)<<s.vardas<<right<<setw(20)<<fixed<<setprecision(2)<<s.vid<<right<<setw(20)<<fixed<<setprecision(2)<<s.med<<endl;
-    }
-}
-
+#include "header.h"
 
 int main() {
     srand(time(NULL));
@@ -88,15 +17,6 @@ int main() {
         cout<<"Pasirinkite:"<<endl;
 
         int  pasirinkimas=s_int();
-
-        if(!(cin>>pasirinkimas)){
-            cin.clear();
-            string kintamasis;
-            getline(cin, kintamasis);
-            cout<<"Ivedete neteisingo tipo duomeni. Bandykite dar karta"<<endl;
-            continue;
-        }
-
 
         if (pasirinkimas==5) {
             cout<<"Programa baigta."<<endl;
