92af591cd70c88b0be282ea5a3dc0d04176cd0ac
GustasNa <gnavierauskas@gmail.com>
2026-05-27 14:24:18 +0300

    Update header.cpp

--- header.cpp
+++ header.cpp
@@ -1,150 +1,31 @@
-#include "header.h"
-
-int s_int()
-{
-    while ( true )
-    {
-        try
-        {
-            int x;
-            cin >> x;
-            return x;
-        }
-        catch ( ... )
-        {
-            cin.clear();
-            string kld;
-            getline ( cin, kld );
-            cout << "Klaida: iveskite skaiciu" << endl;
-        }
-    }
-}
-
-double s_double()
-{
-    while ( true )
-    {
-        try
-        {
-            double x;
-            cin >> x;
-            return x;
-        }
-        catch ( ... )
-        {
-            cin.clear();
-            string kld;
-            getline ( cin, kld );
-            cout << "Klaida: iveskite skaiciu" << endl;
-        }
-    }
-}
-
-double atsitiktinis()
-{
-    return rand() % 11;
-}
-
-void skaiciuoti ( stud &s )
-{
-    double sum = s.egz;
-
-    for ( double x : s.tarp ) sum += x;
-
-    s.vid = sum / ( s.tarp.size() + 1 );
-
-    deque<double> kopija = s.tarp;
-    sort ( kopija.begin(), kopija.end() );
-
-    int d = kopija.size();
-
-    if ( d % 2 == 1 )
-        s.med = kopija[d / 2];
-    else
-        s.med = ( kopija[d / 2 - 1] + kopija[d / 2] ) / 2.0;
-}
-void skaidymas(deque<stud>& A, deque<stud>& varg, deque<stud>& kiet, int pas){
-    if(pas==1){
-            for(const auto &s : A){
-                if (s.vid>=5){
-                    kiet.push_back(s);
-                }
-                else
-                    varg.push_back(s);
-            }
-    }
-    if(pas==2){
-        auto it=A.begin();
-        while(it != A.end()){
-            if(it->vid < 5){
-            varg.push_back(*it);
-               it=A.erase(it);
-            }
-            else{
-                ++it;
-            }
-        }
-    }
-    if(pas==3){
-        auto it=std::partition(A.begin(), A.end(), [](const stud& s){return s.vid<5;});
-
-        varg.insert(varg.end(), A.begin(), it);
-        kiet.insert(kiet.end(), it, A.end());
-    }
-}
+#pragma once
+#include <iostream>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <ctime>
+#include <cctype>
+#include <iomanip>
 
 
-void spausdinti_lentele ( const deque<stud>& A, int pas )
-{
-    ofstream kiet ( "kietekai.txt" );
-    ofstream varg ( "vargsiukai.txt" );
+using namespace std;
 
-    if ( pas == 1 || pas == 3 )
-    {
-        cout << endl << left << setw ( 15 ) << "Pavarde" << setw ( 15 ) << "Vardas" << right << setw ( 20 ) << "Galutinis (Vid.)" << setw ( 20 ) << "Galutinis (Med.)" << endl;
-        cout << string ( 70, '-' ) << endl;
-    }
+int s_int();
+double s_double();
 
-    if ( pas == 2 || pas == 3 )
-    {
-        kiet << left << setw ( 15 ) << "Pavarde" << setw ( 15 ) << "Vardas" << right << setw ( 20 ) << "Galutinis (Vid.)" << setw ( 20 ) << "Galutinis (Med.)" << endl << string ( 70, '-' ) << endl;
+struct stud {
+    string vardas;
+    string pavarde;
+    deque<double> tarp;
+    double egz;
+    double vid;
+    double med;
+};
 
-        varg << left << setw ( 15 ) << "Pavarde" << setw ( 15 ) << "Vardas" << right << setw ( 20 ) << "Galutinis (Vid.)" << setw ( 20 ) << "Galutinis (Med.)" << endl << string ( 70, '-' ) << endl;
-    }
-
-    for ( const auto &s : A )
-    {
-        if ( pas == 1 || pas == 3 )
-        {
-            cout << left << setw ( 15 ) << s.pavarde << setw ( 15 ) << s.vardas << right << setw ( 20 ) << fixed << setprecision ( 2 ) << s.vid << setw ( 20 ) << s.med << endl;
-        }
-
-        if ( s.vid >= 5 )
-            kiet << s.pavarde << " " << s.vardas << " " << s.vid << " " << s.med << endl;
-        else
-            varg << s.pavarde << " " << s.vardas << " " << s.vid << " " << s.med << endl;
-    }
-}
-
-void generuoti_studentus ( int kiekismok, int kiekpaz )
-{
-    ofstream is ( "kursiokai.txt" );
-
-    is << "Vardas Pavarde";
-
-    for ( int i = 1; i <= kiekpaz; i++ )
-        is << " ND" << i;
-
-    is << " Egz." << endl;
-
-    for ( int i = 1; i <= kiekismok; ++i )
-    {
-        is << "Vardas" << i << " Pavarde" << i;
-
-        for ( int j = 0; j < kiekpaz; ++j )
-            is << " " << atsitiktinis();
-
-        is << " " << atsitiktinis() << endl;
-    }
-}
-
+double atsitiktinis();
+void skaiciuoti(stud &s);
+void spausdinti_lentele(const deque<stud>& A, int pas);
+void generuoti_studentus(int kiekismok, int kiekpaz);
+void spausdinti_lentele(deque<stud>& A, deque<stud>& varg, deque<stud>& kiet);
